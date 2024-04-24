f6d4ebb024bc5db11dd2369c8926e3e92287950f
Martins_DUamba <martins.duamba@isutc.ac.mz>
2024-04-24 14:28:35 +0200

    Services configured

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,18 +1,52 @@
-import { HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpContext, HttpHeaders, HttpParams } from '@angular/common/http';
 
-export interface Options{
-  headers?: HttpHeaders | {
-      [header: string]: string | string[];
-  };
-  observe: 'body';
+export interface Options {
+  headers?:
+    | HttpHeaders
+    | {
+        [header: string]: string | string[];
+      };
+  observe?: 'body';
   context?: HttpContext;
-  params?: HttpParams | {
-      [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>;
-  };
+  params?:
+    | HttpParams
+    | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>;
+      };
   reportProgress?: boolean;
   responseType?: 'json';
   withCredentials?: boolean;
-  transferCache?: {
-      includeHeaders?: string[];
-  } | boolean;
+  transferCache?:
+    | {
+        includeHeaders?: string[];
+      }
+    | boolean;
+}
+
+export interface Product {
+  name: string;
+  price: number;
+  rating: number;
+  image: string;
+}
+export interface Products {
+  items: Product[];
+  total: number;
+  page: number;
+  perPage: number;
+  totalPages: number;
+}
+
+export interface PaginationParams {
+  [param: string]:
+    | string
+    | number
+    | boolean
+    | ReadonlyArray<string | number | boolean>;
+  page: number;
+  perPage: number;
 }
