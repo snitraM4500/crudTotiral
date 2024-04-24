4484609bfe8982719d5d3d1ed208816e15c704e1
Martins_DUamba <martins.duamba@isutc.ac.mz>
2024-04-24 16:47:38 +0200

    Footer and showing category works, now working on making a function add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,54 @@
 import { Component } from '@angular/core';
 import { CategoryService } from '../services/category.service';
-import { Product, Products } from '../types';
+import { Category, Categories } from '../types';
 import { CategoryComponent } from '../components/category/category.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CategoryComponent,CommonModule],
+  imports: [CategoryComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private categoryService: CategoryService) {}
 
-  categories: Product[] = [];
-  ngOnInit() {
+  categories: Category[] = [];
+
+  totalRecords: number = 0;
+  rows: number = 6;
+
+  onPageChange = (event: any) => {
+    this.fetchCategories(event.page, event.rows);
+  };
+
+  fetchCategories = (page: number, perPage: number) => {
     this.categoryService
-      .getCategories('http://localhost:3000/categories', { page: 0, perPage: 5 })
-      .subscribe((categories: Products) => {
+      .getCategories('http://localhost:3000/categories', {
+        page,
+        perPage,
+      })
+      .subscribe((categories: Categories) => {
         this.categories = categories.items;
-        console.log(this.categories)
+        this.totalRecords = categories.total;
       });
+  };
+
+  editCategory = (category: Category) => {
+    console.log('Edit category', category);
+  }
+
+  deleteCategory = (category: Category) => {
+    console.log('Delete category', category);
+  }
+
+  addCategory = (category:Category) => {
+    console.log('Add category');
+  }
+
+  ngOnInit() {
+    this.fetchCategories(0, this.rows);
   }
 }
